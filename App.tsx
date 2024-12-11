a65b2eff028e65d59ce2a2f368097a4ca24c31d4
marcelorafael <marcelorafael23@hotmail.com>
2024-12-11 12:55:21 -0300

    feat: add Homepresentation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-import { Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 // import { ThemeProvider } from 'theme/Theme';
 import { ThemeProvider } from 'styled-components/native';
 
 import light from './src/theme/light';
-import Home from './src/screens/Home';
+import HomePresentation from './src/pages/HomePresentation'
 
 export default function App() {
   return (
     <ThemeProvider theme={light}>
-      <Home />
+      <StatusBar hidden showHideTransition="fade" />
+      <HomePresentation />
     </ThemeProvider>
   );
 }
