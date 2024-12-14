2e77fbb11cffbff6d4afffef544f825f1883ce6a
marcelorafael <marcelorafael23@hotmail.com>
2024-12-14 14:56:28 -0300

    feat: base location ok

--- src/pages/HomePresentation/index.tsx
+++ src/pages/HomePresentation/index.tsx
@@ -8,26 +8,34 @@ import Afternoon from '../../assets/afternoon.jpg'
 import Afternoon2 from '../../assets/afternoon-1.jpg'
 import Morning from '../../assets/morning.jpg'
 import { useEffect, useState } from 'react';
+import Geolocation from '@react-native-community/geolocation';
 
 import GetDatas from '../../services/ConnectApi'
+import { AxiosResponse } from 'axios';
 
 export default function HomePresentation() {
-  const[data, setData] = useState([]);
+  const [data, setData] = useState<AxiosResponse | any>(null);
+  const [location, setLocation] = useState<any>(null);
+
+  async function loadData() {
+
+    await GetDatas(location?.coords?.latitude, location?.coords?.longitude).then(res => setData(res));
+
+  };
 
   useEffect(() => {
-    async function loadData() {
-        GetDatas.then(res => setData(res) );
-    };
+
+    Geolocation.getCurrentPosition(info => setLocation(info));
 
     loadData();
-},[data]);
+  }, [data]);
 
-console.log(JSON.stringify(data));
+  console.log(location);
 
- return (
-   <Home
-    title='Belém-PA'
-    backgroundImg={Night}
-   />
+  return (
+    <Home
+      title='Belém-PA'
+      backgroundImg={Night}
+    />
   );
 }
